523ee9a2af5b5dab7e452b14a3708dc60b724c5b
hxztnxt <hxztnxt@hotmail.com>
2023-04-10 01:50:48 +0200

    impl FromStr for Command enum; just one command implemented

--- src/auth/auth_server/commands.rs
+++ src/auth/auth_server/commands.rs
@@ -1,11 +1,11 @@
-use std::error::Error;
+use std::{error::Error, str::FromStr};
 
 use crate::auth::account::LoginError;
 
 
 #[derive(Debug)]
 pub enum CommandError {
-    CommandNotFound,
+    UnknownCommand,
     NotEnoughPrivilege,
     ExecutionError(&'static str),
     LoginError(LoginError)
@@ -17,6 +17,37 @@ impl From<LoginError> for CommandError {
     }
 }
 
+#[derive(Debug)]
+pub enum Command<'a> {
+    Login,
+    Register,
+    Unban (&'a str)
+}
+
+impl FromStr for Command<'_> {
+    type Err = CommandError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let args:Vec<&str> = s.split(" ").collect();
+        match args[0] {
+            "/login" => Ok(Self::Login),
+            _ => Err(CommandError::UnknownCommand),
+        }
+    }
+}
+
 mod logged_in;
 mod anonymous;
 mod common;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_read_command() {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let comm = Command::from_str(input.as_str()).unwrap();
+        println!("{:?}", comm);
+    }
+}
